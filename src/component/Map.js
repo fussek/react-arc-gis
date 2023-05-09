050ce73b553bb0bc29422d7f4657662a7452303a
patrykfussek <patryk.fussek@gmail.com>
2023-05-09 12:04:10 +0200

    earthquakes geojson

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,17 +1,72 @@
 import React, { useEffect } from 'react';
 import WebMap from '@arcgis/core/WebMap.js';
 import MapView from '@arcgis/core/views/MapView.js';
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer.js';
 
 function Map() {
   useEffect(() => {
     const webmap = new WebMap({
-      basemap: 'topo-vector',
+      basemap: 'gray-vector',
     });
 
+    const renderer = {
+      type: 'simple',
+      field: 'mag',
+      symbol: {
+        type: 'simple-marker',
+        color: 'orange',
+        outline: {
+          color: 'white',
+        },
+      },
+      visualVariables: [
+        {
+          type: 'size',
+          field: 'mag',
+          stops: [
+            {
+              value: 2.5,
+              size: '4px',
+            },
+            {
+              value: 8,
+              size: '40px',
+            },
+          ],
+        },
+      ],
+    };
+
+    const template = {
+      title: 'Earthquake Info',
+      content: 'Magnitude {mag} {type} hit {place} on {time}',
+      fieldInfos: [
+        {
+          fieldName: 'time',
+          format: {
+            dateFormat: 'short-date-short-time',
+          },
+        },
+      ],
+    };
     let view = new MapView({
       map: webmap,
+      center: [-168, 46],
+      zoom: 2,
       container: 'viewDiv',
     });
+
+    const geojsonLayer = new GeoJSONLayer({
+      url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson',
+      copyright: 'USGS Earthquakes',
+      popupTemplate: template,
+      renderer: renderer,
+      orderBy: {
+        field: 'mag',
+      },
+    });
+
+    webmap.add(geojsonLayer);
 
     return () => {
       // close the map view
